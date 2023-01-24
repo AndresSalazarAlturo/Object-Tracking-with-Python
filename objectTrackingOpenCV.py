418d30acb76b86073c215b63a15a80c6efe7387d
Andres Salazar <92761344+AndresSalazarAlturo@users.noreply.github.com>
2023-01-24 12:13:46 -0500

    [UPDATE] Detects new objects and delete objects out of sight

--- objectTrackingOpenCV.py
+++ objectTrackingOpenCV.py
@@ -40,28 +40,57 @@ while(True):
         #cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    for pt in center_points_cur_frame:
-        for pt2 in center_points_prev_frame:
-            distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+    #Only at the beggining 
+    if count <= 2:
+        #Section compares the current frame with the previos frame to know if it has the
+        #same ID or it is a different object
+        for pt in center_points_cur_frame:
+            for pt2 in center_points_prev_frame:
+                distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
 
-            if distance < 20:
-                tracking_objects[track_id] = pt
-                track_id += 1
+                if distance < 20:
+                    tracking_objects[track_id] = pt
+                    track_id += 1
 
-    for object_id, pt in tracking_objects.items():
-        cv2.circle(frame, pt, 5, (0, 0, 255), -1)
-        cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
+    else:
 
+        tracking_objects_copy = tracking_objects.copy()
+        center_points_cur_frame_copy = center_points_cur_frame.copy()
 
-    print("Tracking objects")
-    print(tracking_objects)
+        for object_id, pt2 in tracking_objects_copy.items():
+            object_exist = False
+            for pt in center_points_cur_frame_copy:
+                distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+
+                #Update IDs position
+                if distance < 20:
+                    tracking_objects[object_id] = pt
+                    object_exist = True
+                    if pt in center_points_cur_frame:
+                        center_points_cur_frame.remove(pt)
+                    continue
+            
+            #Remove the ID lost        
+            if not object_exist:
+                tracking_objects.pop(object_id)
     
+        # Add new IDs found
+        for pt in center_points_cur_frame: 
+            tracking_objects[track_id] = pt
+            track_id += 1
+    
+    for object_id, pt in tracking_objects.items():
+            cv2.circle(frame, pt, 5, (0, 0, 255), -1)
+            cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
 
-    print("CUR FRAME")
-    print(center_points_cur_frame)
-
-    print("PREV FRAME")
-    print(center_points_prev_frame)
+#    print("Tracking objects")
+#    print(tracking_objects)
+#
+#    print("CUR FRAME")
+#    print(center_points_cur_frame)
+#
+#    print("PREV FRAME")
+#    print(center_points_prev_frame)
 
     #Show the frames
     cv2.imshow("Frame", frame)
@@ -70,7 +99,7 @@ while(True):
     center_points_prev_frame = center_points_cur_frame.copy()
 
     #When use "0" in waitKey it stops in every single frame
-    key = cv2.waitKey(0)
+    key = cv2.waitKey(1)
     if key == 27:
         break
 
